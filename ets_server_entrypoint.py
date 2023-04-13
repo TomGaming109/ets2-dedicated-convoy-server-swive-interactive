51f6302249c44f47c0877d49d3232b5de50d3bd0
TomGaming109 <87391132+TomGaming109@users.noreply.github.com>
2023-04-13 14:07:15 +0100

    Update ets_server_entrypoint.py

--- ets_server_entrypoint.py
+++ ets_server_entrypoint.py
@@ -119,7 +119,7 @@ server_config : _nameless.44c.eab0 {{
 if __name__ == "__main__":
     if is_truthy(os.getenv("ETS_SERVER_WRITE_CONFIG", "true")):
         config = generate_config()
-        config_path = os.getenv("ETS_SERVER_CONFIG_FILE_PATH", "/root/.local/share/Euro Truck Simulator 2/server_config.sii")
+        config_path = os.getenv("ETS_SERVER_CONFIG_FILE_PATH", "/home/container/EuroTruckSimulator2/server_config.sii")
         with open(config_path, "w") as f:
             if f.writable():
                 f.write(config)
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     if is_truthy(os.getenv("ETS_SERVER_UPDATE_ON_START", "true")) or not server_files_exist():
         print("[INFO]: Updating ETS Server...")
         APP_ID = os.getenv("APP_ID")
-        os.system(f"steamcmd +force_install_dir /app +login anonymous +app_update {APP_ID} +quit")
+        os.system(f"steamcmd +force_install_dir /home/container +login anonymous +app_update {APP_ID} +quit")
         print("[INFO]: Update done.")
     else:
         print("[INFO]: Skipping server update. To update set 'ETS_SERVER_UPDATE_ON_START=true'.")
