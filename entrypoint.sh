97d215c7a383992026c702a4c170f39b244e6cc6
TomGaming109 <87391132+TomGaming109@users.noreply.github.com>
2023-04-13 14:13:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 # Generate config and update server
 # I can't bash so I wrote a python script
-/usr/bin/python3 /ets_server_entrypoint.py
+/usr/bin/python3 /home/container/ets_server_entrypoint.py
 
 # Copy default server_packages if they do not exist
 mkdir "/home/container/EuroTruckSimulator2/"
-cp -n /default_packages/server_packages.sii "$/home/container/EuroTruckSimulator2/"
+cp -n /default_packages/server_packages.sii "/home/container/EuroTruckSimulator2/"
 cp -n /default_packages/server_packages.dat "/home/container/EuroTruckSimulator2/"
 
 echo "[INFO]: Starting server..."
