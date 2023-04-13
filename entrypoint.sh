32b0b848a7c2b63ceb2e4f8b2e5bc506567209ea
TomGaming109 <87391132+TomGaming109@users.noreply.github.com>
2023-04-13 14:52:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ cp -n /default_packages/server_packages.dat "/home/container/EuroTruckSimulator2
 cd /home/container
 mkdir /home/container/steamcmd
 cd /home/container/steamcmd
-wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+curl -O http://media.steampowered.com/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
 ./steamcmd +login anonymous +force_install_dir /home/container +app_update 1948160 +quit
 
